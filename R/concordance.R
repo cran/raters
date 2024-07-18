cb2c3645b5f2304efb2a1e3c6acd6d41745e19c3
Daniele Giardiello <daniele.giardiello1@gmail.com>
2024-07-18 07:20:02 +0000

    version 2.1.0

--- R/concordance.R
+++ R/concordance.R
@@ -1,109 +1,94 @@
 concordance <- function(db, test = "Default", B = 1000, alpha = 0.05) {
+  db <- as.matrix(db)
   db2 <- db * (db - 1)
+  R <- nrow(db)
+  C <- ncol(db)
   sumcol <- colSums(db)
   sumrow <- rowSums(db)
   tot <- sum(db)
   vet <- list()
-  pij <- matrix(, nrow = nrow(db), ncol = ncol(db))
-  for (i in 1:length(sumrow)) {
-    for (j in 1:length(sumcol)) {
-      pij[i, j] <- db2[i, j] / (sumrow[i] * (sumrow[i] -
-        1))
-    }
-  }
+  pij <- matrix(NA, nrow = R, ncol = C)
+
+  pij <- db2 / (sumrow * (sumrow - 1))
+
   pi <- rowSums(pij)
   p <- mean(pi)
   pj <- sumcol / tot
   pj2 <- pj^2
   pe <- sum(pj2)
   fleiss.kappa <- (p - pe) / (1 - pe)
-  s <- (ncol(db) * p - 1) / (ncol(db) - 1)
+  s <- (C * p - 1) / (C - 1)
   s.boot <- c()
   pi.v.boot <- replicate(B, pi.boot <- sample(pi,
-    size = nrow(db),
-    replace = TRUE
+                                              size = R,
+                                              replace = TRUE
   ))
   p.boot <- apply(pi.v.boot, 2, mean)
-  for (i in 1:B) {
-    s.boot[i] <- (ncol(db) * p.boot[i] - 1) / (ncol(db) - 1)
-  }
+  s.boot <- (C * p.boot - 1) / (C - 1)
   s.boot.ci <- quantile(s.boot, probs = c(alpha / 2, 1 - alpha / 2))
+
   Default <- function(vet) {
     s.vet <- c(s, s.boot.ci)
     names(s.vet) <- c("S", "LCL", "UCL")
     vet <- list(Fleiss = fleiss.kappa, Statistic = s.vet)
     cat(paste("Inter-rater Agreement"), "\n")
+    message("Fleiss kappa confidence intervals and test not estimated since the number of raters is not the same among evaluated subjects (n)")
     vet
   }
+
   Normal <- function(db) {
-    stat.test <- s * nrow(db) * sqrt((ncol(db) - 1) / (2 *
-      sum(1 / (rowSums(db) * (rowSums(db) - 1)))))
+    stat.test <- s * R * sqrt((C - 1) / (2 * sum(1 / (sumrow * (sumrow - 1)))))
     bin <- stat.test > qnorm(1 - alpha)
     pvalue <- 1 - pnorm(stat.test)
     s.vet <- c(s, s.boot.ci, pvalue)
     names(s.vet) <- c("S", "LCL", "UCL", "pvalue")
     vet <- list(Fleiss = fleiss.kappa, Statistic = s.vet)
     cat(paste("Inter-rater Agreement"), "\n")
+    message("Fleiss kappa confidence intervals and test not estimated since the number of raters is not the same among evaluated subjects (n)")
     vet
   }
+
+  Chisq <- function(db)
+    {stop("Chi-squared test approximation can be used only when total number of raters is the same among evaluated subjects (n)")}
+
   MC <- function(db) {
-    matrix.mc <- replicate(B, matrix.rep <- matrix(,
-      nrow = nrow(db),
-      ncol = ncol(db)
-    ))
-    pij.mc <- replicate(B, pij.rep <- matrix(,
-      nrow = nrow(db),
-      ncol = ncol(db)
-    ))
-    for (k in 1:B) {
-      for (j in 1:nrow(db)) {
-        matrix.mc[j, , k] <- t(rmultinom(1,
-          size = rowSums(db)[j], prob = rep(
-            1 / (ncol(db)),
-            ncol(db)
-          )
-        ))
-      }
-    }
-    for (k in 1:B) {
-      for (i in 1:nrow(db)) {
-        for (j in 1:ncol(db)) {
-          pij.mc[i, j, k] <- matrix.mc[i, j, k] * (matrix.mc[
-            i,
-            j, k
-          ] - 1) / (rowSums(db)[i] * (rowSums(db)[i] -
-            1))
-        }
-      }
-    }
-    pi.mc <- list()
-    for (i in 1:B) {
-      pi.mc[[i]] <- apply(pij.mc[, , i], 1, sum)
-    }
-    p.mc <- c()
-    for (k in 1:B) {
-      p.mc[k] <- mean(pi.mc[[k]])
-    }
-    s.mc <- c()
-    for (k in 1:B) {
-      s.mc[k] <- ((p.mc[k] * ncol(db)) - 1) / (ncol(db) -
-        1)
-    }
+
+    matrix.mc <- lapply(1:B,
+                        matrix,
+                        data = NA,
+                        nrow = R,
+                        ncol = C)
+
+    matrix.mc <- lapply(matrix.mc,
+                               function(list) {
+                                 for (j in 1:R) {
+                                   list[j, ] <- t(rmultinom(1, size = rowSums(db)[j], prob = rep(1 / C, C)))
+                                 }
+                                 return(list)
+                               })
+
+    pij.mc <- lapply(matrix.mc,
+                            function(list) {
+                              list * (list - 1) / (sumrow * (sumrow - 1))
+                            })
+
+    pi.mc <- lapply(pij.mc, function(list) {apply(list, 1, sum)})
+
+    p.mc <- do.call(rbind, lapply(pi.mc,mean))
+    s.mc <- ((p.mc * C) - 1) / (C - 1)
     crit.s.mc <- quantile(s.mc, 0.95)
-    binary <- c()
-    for (i in 1:length(s.mc)) {
-      binary[i] <- (s.mc[i] >= s)
-    }
+    binary <- (s.mc >= s)
     pvalue <- sum(binary) / B
     s.vet <- c(s, s.boot.ci, pvalue)
     names(s.vet) <- c("S", "LCL", "UCL", "pvalue")
     vet <- list(Fleiss = fleiss.kappa, Statistic = s.vet)
     cat(paste("Inter-rater Agreement"), "\n")
+    message("Fleiss kappa confidence intervals and test not estimated since the number of raters is not the same among evaluated subjects (n)")
     vet
   }
+
   if (length(unique(rowSums(db))) == 1) {
-    std.kappaj <- sqrt(2 / (nrow(db) * (rowSums(db)[1] * (rowSums(db)[1] -
-      1))))
+    std.kappaj <- sqrt(2 / (R * (sumrow[1] * (sumrow[1] - 1))))
     pj.1 <- 1 - pj
     pj.2 <- 1 - 2 * pj
     std.num <- sqrt((sum(pj * pj.1))^2 - sum(pj * pj.1 * pj.2))
@@ -114,24 +99,51 @@ concordance <- function(db, test = "Default", B = 1000, alpha = 0.05) {
     ci.upp <- fleiss.kappa + qnorm(1 - alpha / 2) * std.kappa
     z.fleiss <- fleiss.kappa / std.kappa
     pvalue.fleiss <- 1 - pnorm(z.fleiss)
-    if (ci.upp > 1 | ci.low < (-1 / (rowSums(db)[1] - 1))) {
-      ci.low <- (1 / (rowSums(db)[1] - 1))
+    if (ci.upp > 1 | ci.low < (-1 / (sumrow[1] - 1))) {
+      ci.low <- (1 / (sumrow[1] - 1))
       ci.upp <- 1
     }
-    Chisq <- function(db) {
-      stat.test <- nrow(db) * (ncol(db) - 1) * ((rowSums(db) -
-        1)[1] * s + 1)
-      bin <- stat.test > qchisq(alpha, df = (nrow(db) *
-        (ncol(db) - 1)))
-      pvalue <- 1 - pchisq(stat.test, df = (nrow(db) *
-        (ncol(db) - 1)))
+
+    Default2 <- function(vet) {
       kappa.vet <- c(
-        fleiss.kappa, ci.low, ci.upp, std.kappa,
-        z.fleiss, pvalue.fleiss
+        fleiss.kappa,
+        ci.low,
+        ci.upp,
+        std.kappa
       )
       names(kappa.vet) <- c(
-        "Kappa", "LCL", "UCL", "Std.Error",
-        "Z value", "Pr(>|z|)"
+        "Kappa",
+        "LCL",
+        "UCL",
+        "Std.Error"
+      )
+
+      s.vet <- c(s, s.boot.ci)
+      names(s.vet) <- c("S", "LCL", "UCL")
+      vet <- list(Fleiss = kappa.vet, Statistic = s.vet)
+      cat(paste("Inter-rater Agreement"), "\n")
+      vet
+    }
+
+    Chisq2 <- function(db) {
+      stat.test <- R * (C - 1) * ((sumrow -1)[1] * s + 1)
+      bin <- stat.test > qchisq(alpha, df = R *(C - 1))
+      pvalue <- 1 - pchisq(stat.test, df = R *(C - 1))
+      kappa.vet <- c(
+        fleiss.kappa,
+        ci.low,
+        ci.upp,
+        std.kappa,
+        z.fleiss,
+        pvalue.fleiss
+      )
+      names(kappa.vet) <- c(
+        "Kappa",
+        "LCL",
+        "UCL",
+        "Std.Error",
+        "Z value",
+        "Pr(>|z|)"
       )
       s.vet <- c(s, s.boot.ci, pvalue)
       names(s.vet) <- c("S", "LCL", "UCL", "pvalue")
@@ -139,18 +151,26 @@ concordance <- function(db, test = "Default", B = 1000, alpha = 0.05) {
       cat(paste("Inter-rater Agreement"), "\n")
       vet
     }
+
     Normal2 <- function(db) {
-      stat.test <- s * nrow(db) * sqrt((ncol(db) - 1) / (2 *
-        sum(1 / (rowSums(db) * (rowSums(db) - 1)))))
+      stat.test <- s * R * sqrt((C - 1) / (2 * sum(1 / (sumrow * (sumrow - 1)))))
       bin <- stat.test > qnorm(1 - alpha)
       pvalue <- 1 - pnorm(stat.test)
       kappa.vet <- c(
-        fleiss.kappa, ci.low, ci.upp, std.kappa,
-        z.fleiss, pvalue.fleiss
+        fleiss.kappa,
+        ci.low,
+        ci.upp,
+        std.kappa,
+        z.fleiss,
+        pvalue.fleiss
       )
       names(kappa.vet) <- c(
-        "Kappa", "LCL", "UCL", "Std.Error",
-        "Z value", "Pr(>|z|)"
+        "Kappa",
+        "LCL",
+        "UCL",
+        "Std.Error",
+        "Z value",
+        "Pr(>|z|)"
       )
       s.vet <- c(s, s.boot.ci, pvalue)
       names(s.vet) <- c("S", "LCL", "UCL", "pvalue")
@@ -158,54 +178,52 @@ concordance <- function(db, test = "Default", B = 1000, alpha = 0.05) {
       cat(paste("Inter-rater Agreement"), "\n")
       vet
     }
+
     MC2 <- function(db) {
-      matrix.mc <- replicate(B, matrix.rep <- t(rmultinom(nrow(db),
-        size = rowSums(db)[1], prob = rep(
-          1 / (ncol(db)),
-          ncol(db)
-        )
-      )))
-      pij.mc <- replicate(B, pij.rep <- matrix(,
-        nrow = nrow(db),
-        ncol = ncol(db)
-      ))
-      for (k in 1:B) {
-        for (i in 1:nrow(db)) {
-          for (j in 1:ncol(db)) {
-            pij.mc[i, j, k] <- matrix.mc[i, j, k] * (matrix.mc[
-              i,
-              j, k
-            ] - 1) / (rowSums(db)[i] * (rowSums(db)[i] -
-              1))
-          }
-        }
-      }
-      pi.mc <- list()
-      for (i in 1:B) {
-        pi.mc[[i]] <- apply(pij.mc[, , i], 1, sum)
-      }
-      p.mc <- c()
-      for (k in 1:B) {
-        p.mc[k] <- mean(pi.mc[[k]])
-      }
-      s.mc <- c()
-      for (k in 1:B) {
-        s.mc[k] <- ((p.mc[k] * ncol(db)) - 1) / (ncol(db) -
-          1)
-      }
+
+      matrix.mc <- lapply(1:B,
+                          matrix,
+                          data = NA,
+                          nrow = R,
+                          ncol = C)
+
+      matrix.mc <- lapply(matrix.mc,
+                          function(list) {
+                            for (j in 1:R) {
+                              list[j, ] <- t(rmultinom(1, size = rowSums(db)[j], prob = rep(1 / C, C)))
+                            }
+                            return(list)
+                          })
+
+      pij.mc <- lapply(matrix.mc,
+                       function(list) {
+                         list * (list - 1) / (sumrow * (sumrow - 1))
+                       })
+
+      pi.mc <- lapply(pij.mc, function(list) {apply(list, 1, sum)})
+
+      p.mc <- do.call(rbind, lapply(pi.mc,mean))
+      s.mc <- ((p.mc * C) - 1) / (C - 1)
       crit.s.mc <- quantile(s.mc, 0.95)
-      binary <- c()
-      for (i in 1:length(s.mc)) {
-        binary[i] <- (s.mc[i] >= s)
-      }
+      binary <- (s.mc >= s)
       pvalue <- sum(binary) / B
+
       kappa.vet <- c(
-        fleiss.kappa, ci.low, ci.upp, std.kappa,
-        z.fleiss, pvalue.fleiss
+        fleiss.kappa,
+        ci.low,
+        ci.upp,
+        std.kappa,
+        z.fleiss,
+        pvalue.fleiss
       )
+
       names(kappa.vet) <- c(
-        "Kappa", "LCL", "UCL", "Std.Error",
-        "Z value", "Pr(>|z|)"
+        "Kappa",
+        "LCL",
+        "UCL",
+        "Std.Error",
+        "Z value",
+        "Pr(>|z|)"
       )
       s.vet <- c(s, s.boot.ci, pvalue)
       names(s.vet) <- c("S", "LCL", "UCL", "pvalue")
@@ -213,17 +231,24 @@ concordance <- function(db, test = "Default", B = 1000, alpha = 0.05) {
       cat(paste("Inter-rater Agreement"), "\n")
       vet
     }
+
     switch(test,
-      Normal = Normal2(db),
-      MC = MC2(db),
-      Chisq = Chisq(db),
-      Default = Default(vet)
+           Normal = Normal2(db),
+           MC = MC2(db),
+           Chisq = Chisq2(db),
+           Default = Default2(vet)
     )
-  } else {
+  }
+  else {
     switch(test,
-      Normal = Normal(db),
-      MC = MC(db),
-      Default = Default(vet)
+           Normal = Normal(db),
+           MC = MC(db),
+           Chisq = Chisq(db),
+           Default = Default(vet)
     )
   }
 }
+
+
+
+
--- R/wlin.conc.R
+++ R/wlin.conc.R
@@ -1,20 +1,18 @@
 wlin.conc <-
   function(db, test = "Default", B = 1000, alpha = 0.05) {
+    db <- as.matrix(db)
     C <- ncol(db)
     R <- nrow(db)
     row.sum <- rowSums(db)
     db2 <- db^2
     row.sum2 <- rowSums(db2)
 
-    w <- matrix(, nrow = C, ncol = C)
+    w <- matrix(NA, nrow = C, ncol = C)
     for (j in 1:C) {
       for (k in 1:C) {
         w[j, k] <- 1 - (abs(j - k) / (C - 1))
       }
     }
-
-
-
 
     ww <- rep(0, R)
 
@@ -27,37 +25,26 @@ wlin.conc <-
       }
     }
 
-    xi <- c()
-    pi <- c()
-    for (h in 1:R) {
-      xi[h] <- (0.5 * row.sum2[h]) + ww[h] - (0.5 * row.sum[h])
-      pi[h] <- (2 * xi[h]) / (row.sum[h] * (row.sum[h] - 1))
-    }
+    xi <- (0.5 * row.sum2) + ww - (0.5 * row.sum)
+    pi <- (2 * xi) / (row.sum * (row.sum - 1))
 
     p.avg <- mean(pi)
     pe <- (1 / (C^2)) * sum(w)
 
     s.star <- (p.avg - pe) / (1 - pe)
 
-
-    obj.min <- c()
-    for (i in 1:R) {
-      obj.min[i] <- (row.sum[i] - 2) / (row.sum[i] - 1)
-    }
+    obj.min <- (row.sum - 2) / (row.sum - 1)
     sum.obj.min <- sum(obj.min) * ((3 * C) / (2 * R))
 
     min.s <- (sum.obj.min - 2 * C + 1) / (C + 1)
 
-
     s.star
     pi.boot.w <- list()
-    p.boot.w <- c()
-    s.boot.w <- c()
     for (i in 1:B) {
       pi.boot.w[[i]] <- sample(pi, size = nrow(db), replace = TRUE)
-      p.boot.w[i] <- mean(pi.boot.w[[i]])
-      s.boot.w[i] <- (p.boot.w[i] - pe) / (1 - pe)
     }
+    p.boot.w <- do.call(rbind, lapply(pi.boot.w, mean))
+    s.boot.w <- (p.boot.w - pe) / (1 - pe)
     s.boot.ci.w <- quantile(s.boot.w, probs = c(alpha / 2, 1 - alpha / 2))
 
 
@@ -67,37 +54,31 @@ wlin.conc <-
       s.res
     }
 
-
+    set.seed(1234)
     MC <- function(db) {
-      matrix.mc <- list()
-      matrix.mc2 <- list()
       R <- nrow(db)
       C <- ncol(db)
-      w.sum <- list()
-      rs.mc <- list()
-      rs2.mc <- list()
-      xi.mc <- list()
-      pi.mc <- list()
+      w.sum <- lapply(1:B, function(x) {rep(0, R)})
+      xi.mc <- do.call(rbind, w.sum)
+      pi.mc <- xi.mc
 
-      for (h in 1:B) {
-        matrix.mc[[h]] <- matrix(,
-          nrow = R, ncol = C,
-          byrow = T
-        )
-        matrix.mc2[[h]] <- matrix(, nrow = R, ncol = C, byrow = T)
-        w.sum[[h]] <- rep(0, R)
-        xi.mc[[h]] <- rep(0, R)
-        pi.mc[[h]] <- rep(0, R)
-      }
-      for (k in 1:B) {
-        for (j in 1:R) {
-          matrix.mc[[k]][j, ] <- t(rmultinom(1, size = rowSums(db)[j], prob = rep(1 / C, C)))
-          matrix.mc2[[k]] <- (matrix.mc[[k]])^2
-          rs.mc[[k]] <- rowSums(matrix.mc[[k]])
-          rs2.mc[[k]] <- rowSums(matrix.mc2[[k]])
-        }
-      }
+      matrix.mc <- lapply(1:B,
+                          matrix,
+                          data = NA,
+                          nrow = R,
+                          ncol = C)
 
+      matrix.mc <- lapply(matrix.mc,
+                          function(list) {
+                            for (j in 1:R) {
+                              list[j, ] <- t(rmultinom(1, size = rowSums(db)[j], prob = rep(1 / C, C)))
+                            }
+                            return(list)
+                          })
+
+      matrix.mc2 <- lapply(matrix.mc, function(x) {x^2})
+      rs.mc <- do.call(rbind, lapply(matrix.mc, rowSums))
+      rs2.mc <- do.call(rbind, lapply(matrix.mc2, rowSums))
 
       for (g in 1:B) {
         for (i in 1:R) {
@@ -110,30 +91,27 @@ wlin.conc <-
         }
       }
 
-      p.avg.mc <- c()
-      pe.mc <- c()
-      s.star.mc <- c()
-      for (g in 1:B) {
-        for (h in 1:R) {
-          xi.mc[[g]][h] <- (0.5 * rs2.mc[[g]][h]) + w.sum[[g]][h] - (0.5 * rs.mc[[g]][h])
-          pi.mc[[g]][h] <- (2 * xi.mc[[g]][h]) / (rs.mc[[g]][h] * (rs.mc[[g]][h] - 1))
-          p.avg.mc[g] <- mean(pi.mc[[g]])
-          pe.mc[g] <- (1 / (C^2)) * sum(w)
-          s.star.mc[g] <- (p.avg.mc[g] - pe.mc[g]) / (1 - pe.mc[g])
-        }
-      }
+      w.sum <- do.call(rbind, w.sum)
+
+      xi.mc <- (0.5 * rs2.mc)  + w.sum - (0.5 * rs.mc)
+      pi.mc <- (2 * xi.mc) / (rs.mc *(rs.mc - 1))
+      p.avg.mc <- rowMeans(pi.mc)
+      pe.mc <- (1 / (C^2)) * sum(w)
+      s.star.mc <- (p.avg.mc - pe.mc) / (1 - pe.mc)
+
       crit.s.mc <- quantile(s.star.mc, 0.95)
-      binary <- c()
-      for (i in 1:length(s.star.mc)) {
-        binary[i] <- (s.star.mc[i] >= s.star)
-      }
+      binary <- s.star.mc >= s.star
       pvalue <- sum(binary) / B
       s.res <- c(s.star, min.s, s.boot.ci.w, pvalue)
       names(s.res) <- c("S*", "min", "LCL", "UCL", "pvalue")
       s.res
     }
     switch(test,
-      Default = Default(db),
-      MC = MC(db)
+           Default = Default(db),
+           MC = MC(db)
     )
   }
+
+
+
+
